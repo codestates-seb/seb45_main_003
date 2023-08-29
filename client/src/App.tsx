88b867d012725e477e4335623f1583a82217866e
dahae <eg1377@naver.com>
2023-08-29 23:20:32 +0900

    [Add] 파일, 라이프러리 추가 셋팅
    1) 상태 관리 (Recoil,React Query)
    2) 라이브러리 (react-hook-form)
    3) Pages 파일
    4) Components > common 파일
    추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,80 @@
+import type { RouteObject } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import Main from "./pages/Main";
+import PostInformation from "./pages/PostInformation";
+import Profile from "./pages/Profile";
+import Review from "./pages/Review";
+import CreatePost from "./pages/CreatePost";
+import ErrorIndication from "./pages/ErrorIndication";
+import Chatting from "./pages/Chatting";
+import PostsList from "./pages/PostsList";
+import LogIn from "./pages/LogIn";
+
 function App() {
-  return <div className="App">test</div>;
+  const routes: RouteObject[] = [
+    // 1) 메인 홈 페이지
+    {
+      path: "/",
+      element: <Main />,
+    },
+
+    // 2) 로그인 페이지
+    {
+      path: "/login",
+      element: <LogIn />,
+    },
+
+    // 3) 프로필 페이지
+    {
+      path: "/member/:id",
+      element: <Profile />,
+    },
+
+    // 4) 게시물 작성
+    {
+      path: "/create-post",
+      element: <CreatePost />,
+    },
+
+    // 5) 게시물 리스트 페이지
+    {
+      path: "/product",
+      element: <PostsList />,
+    },
+
+    // 6) 게시글 상세 페이지
+    {
+      path: "/product/:item",
+      element: <PostInformation />,
+    },
+
+    // 7) 채팅 페이지
+    {
+      path: "/chat/:id",
+      element: <Chatting />,
+    },
+
+    // 8) 후기,리뷰 페이지
+    {
+      path: "/review/:id",
+      element: <Review />,
+    },
+
+    // 9) 에러 페이지
+    {
+      path: "*",
+      element: <ErrorIndication />,
+    },
+  ];
+
+  const router = createBrowserRouter(routes);
+
+  return (
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
 }
 
 export default App;
---
+++ client/src/pages/Review.tsx
@@ -0,0 +1,9 @@
+const Review = (): JSX.Element => {
+  return (
+    <>
+      <h1>Review</h1>
+    </>
+  );
+};
+
+export default Review;
