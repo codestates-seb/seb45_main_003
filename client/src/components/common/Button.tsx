3cc66494968c2684f18b261200e53426b04ff569
pearlsea <sakanaplanet@gmail.com>
2023-08-30 18:31:26 +0900

    [Feat] 공통 컴포넌트 작성

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,5 +1,10 @@
-const Button = (): JSX.Element => {
-  return <></>;
+interface ButtonProp {
+  buttonText: string;
+  onClick?: (event?: React.MouseEvent<HTMLElement>) => void;
+}
+
+const Button = ({ buttonText, ...props }: ButtonProp): JSX.Element => {
+  return <button {...props}>{buttonText}</button>;
 };
 
 export default Button;
