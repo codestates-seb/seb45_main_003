b93941b2c777168e9af23593c8ec423728b2814b
pearlsea <sakanaplanet@gmail.com>
2023-08-30 11:10:05 +0900

    [Design] 전역 폰트 설정

---
+++ client/src/fonts.d.ts
@@ -0,0 +1,4 @@
+declare module "*.woff" {
+  const content: string;
+  export default content;
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GlobalFont } from "./styles/font";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
+    <GlobalFont />
     <App />
   </React.StrictMode>,
 );
