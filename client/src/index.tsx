0476d4d72e3fbbd3e866bc757ad6e9bbe4fc601f
pearlsea <sakanaplanet@gmail.com>
2023-08-30 11:13:55 +0900

    [Design] css reset 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { GlobalFont } from "./styles/font";
+import { GlobalStyle } from "./styles/global";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <React.StrictMode>
     <GlobalFont />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
 );
