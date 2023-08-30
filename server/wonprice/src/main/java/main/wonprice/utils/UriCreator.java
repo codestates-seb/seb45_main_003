2c8a7f25a1a8228ef6a36ac147f05f8f206e4cfd
Jeonni <jeonniu@naver.com>
2023-08-30 16:55:24 +0900

    Feat : UriCreator 추가

---
+++ server/wonprice/src/main/java/main/wonprice/utils/UriCreator.java
@@ -0,0 +1,15 @@
+package main.wonprice.utils;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+public class UriCreator {
+    public static URI createUri(String defaultUrl, Long resourceId) {
+        return UriComponentsBuilder
+                .newInstance()
+                .path(defaultUrl + "/{resource-id}")
+                .buildAndExpand(resourceId)
+                .toUri();
+    }
+}
