57bd9dc10acf7471624db0372dbfce796b617055
Jeonni <jeonniu@naver.com>
2023-09-01 03:15:28 +0900

    Feat : 경매 선택 시, Product 생성 로직 개선

--- server/wonprice/src/main/java/main/wonprice/domain/product/mapper/ProductMapper.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/mapper/ProductMapper.java
@@ -12,7 +12,7 @@ public interface ProductMapper {
 
     // DTO -> Entity
     default Product toEntity(ProductRequestDto productRequestDto, Member member) {
-        return Product.builder()
+        Product.ProductBuilder productBuilder = Product.builder()
                 .seller(member)
                 .title(productRequestDto.getTitle())
                 .description(productRequestDto.getDescription())
@@ -20,8 +20,16 @@ public interface ProductMapper {
                 .auction(productRequestDto.getAuction())
                 .status(ProductStatus.BEFORE)
                 .views(0L)
-                .removed(false) // 게시글 삭제 여부 셋팅값은 false
-                .build();
+                .removed(false);
+
+        // auction이 true인 경우에만 추가 정보 설정
+        if (productRequestDto.getAuction()) {
+            productBuilder
+                    .currentAuctionPrice(productRequestDto.getCurrentAuctionPrice())
+                    .closedAt(productRequestDto.getClosedAt());
+        }
+
+        return productBuilder.build();
     }
 
     // Entity -> DTO
@@ -39,6 +47,8 @@ public interface ProductMapper {
                 .modifiedAt(product.getModifiedAt())
                 .deletedAt(product.getDeletedAt())
                 .removed(product.getRemoved())
+                .currentAuctionPrice(product.getCurrentAuctionPrice())
+                .closedAt(product.getClosedAt())
                 .build();
     }
 }
