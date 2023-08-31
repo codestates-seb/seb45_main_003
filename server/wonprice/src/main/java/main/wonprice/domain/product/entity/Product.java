6dfc7f8cc8dc61b8a50d5427bcabc2c11fc54101
Jeonni <jeonniu@naver.com>
2023-09-01 00:58:34 +0900

    Feat : 게시글 삭제 여부를 추적하는 removed 필드 추가

--- server/wonprice/src/main/java/main/wonprice/domain/product/entity/Product.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/entity/Product.java
@@ -17,39 +17,41 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
-    private Long buyerId;
+    private Long buyerId; // 구매자
 
-    private String title;
+    private String title; // 글 제목
 
     @Lob
-    private String description;
+    private String description; // 내용
 
-    private Long immediatelyBuyPrice;
+    private Long immediatelyBuyPrice; // 즉시 구매가
 
-    private Long currentAuctionPrice;
+    private Long currentAuctionPrice; // 현재 경매가
 
-    private Boolean auction;
+    private Boolean auction; // 경매 여부
 
-    private LocalDateTime createAt;
+    private LocalDateTime createAt; // 거래 시작 시간 (작성시간)
 
-    private LocalDateTime deletedAt;
+    private LocalDateTime deletedAt; // 글 삭제 시간
 
-    private LocalDateTime modifiedAt;
+    private LocalDateTime modifiedAt; // 글 수정 시간
 
     @Enumerated(EnumType.STRING)
-    private ProductStatus status;
+    private ProductStatus status; // 상품 거래 상태
 
-    private Long views;
+    private Long views; // 조회수
 
-    private LocalDateTime closedAt;
+    private LocalDateTime closedAt; // 경매 종료 시간
 
     @OneToOne
     @JoinColumn(name = "category_id")
-    private Category category;
+    private Category category; // 카테고리
 
     @ManyToOne
     @JoinColumn(name = "member_id")
-    private Member seller;
+    private Member seller; // 판매자
+    
+    private Boolean removed; // 글 삭제 여부 (게시글 삭제 시, 실제 DB에서 삭제되는 것 X true, false 로 명시)
 
     @Builder
     public Product(Member seller, String title, String description, Long immediatelyBuyPrice) {
