a03437df0e6257f6e5db39ceb9297e685a5c60d5
Jeonni <jeonniu@naver.com>
2023-08-31 16:02:14 +0900

    Feat : 상품 조회수 카운팅 서비스 코드 추가

--- server/wonprice/src/main/java/main/wonprice/domain/product/service/ProductServiceImpl.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/service/ProductServiceImpl.java
@@ -16,18 +16,30 @@ public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
 
+    // 상품 등록
     @Override
     public Product save(Product product) {
         return productRepository.save(product);
     }
 
+    // 전체 상품
     @Override
     public List<Product> findAll() {
         return productRepository.findAll();
     }
 
+    // 특정 상품
     @Override
-    public Optional<Product> findOneById(Long productId) {
-        return productRepository.findById(productId);
+    public Product findOneById(Long productId) {
+        Product product = findExistsProduct(productId);
+        product.setViews(product.getViews() + 1); // 조회수 카운팅
+        return productRepository.save(product);
+    }
+
+    // 상품이 존재하는지 확인하는 메서드
+    @Override
+    public Product findExistsProduct(Long productId) {
+        Optional<Product> product = productRepository.findById(productId);
+        return product.orElseThrow();
     }
 }
