a23361cde1d786770ec06278ba134c6182061de1
pearlsea <sakanaplanet@gmail.com>
2023-08-31 14:38:05 +0900

    design: 헤딩 텍스트 기본 폰트 사이즈 추가

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -36,13 +36,20 @@ ul {
   padding: 0;
   letter-spacing: -0.1px;
 }
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-  font-size: 100%;
+h1 {
+  font-size: 2rem;
+}
+h2 {
+  font-size: 1.5rem;
+}
+h3 {
+  font-size: 1.25rem;
+}
+h4 {
+  font-size: 1.125rem;
+}
+h5 {
+  font-size: 1rem;
 }
 ul {
   list-style: none;
