8de61d1e5b1b9af62979814dc98669ccded2b554
dahae <eg1377@naver.com>
2023-08-31 21:58:18 +0900

    [Add] Carousel 작업중

--- client/src/components/Swiper/Carousel.tsx
+++ client/src/components/Swiper/Carousel.tsx
@@ -4,14 +4,24 @@ import { SwiperProps as ReactSwiperProps, Swiper, SwiperSlide } from "swiper/rea
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import styled from "styled-components";
 
 import "../Swiper/styles.css";
+import { Navigation } from "swiper/modules";
 
+// SwiperCore.use([Navigation, Pagination]);
+
+const Box = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: aqua;
+`;
 interface CustomSwiperProps extends ReactSwiperProps {
   slidesPerView?: number;
   spaceBetween?: number;
   pagination?: { clickable: boolean };
   navigation?: boolean;
+  modules?: object;
 }
 const Carousel = (): JSX.Element => {
   const swiperProps: CustomSwiperProps = {
@@ -24,17 +34,21 @@ const Carousel = (): JSX.Element => {
   };
   return (
     <>
-      <Swiper className="mySwiper" {...swiperProps}>
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+      <div id="app">
+        <Swiper modules={[Navigation]} className="mySwiper" {...swiperProps}>
+          <SwiperSlide>
+            <Box>Slide 1</Box>
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 };
---
+++ client/src/components/Swiper/Item.tsx
@@ -0,0 +1,9 @@
+const ItemImg = (): JSX.Element => {
+  return (
+    <>
+      <img src="client/src/assets/images/Img1.png"></img>
+    </>
+  );
+};
+
+export default ItemImg;
