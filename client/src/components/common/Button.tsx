1cff956fb83298a23b988b071730ab48b5b5096a
pearlsea <sakanaplanet@gmail.com>
2023-08-31 09:37:26 +0900

    feat: 버튼 컴포넌트 prop정리

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,10 +1,16 @@
 interface ButtonProp {
+  type: "button" | "submit" | "reset" | undefined;
   buttonText: string;
+  disabled: boolean;
   onClick?: (event?: React.MouseEvent<HTMLElement>) => void;
 }
 
-const Button = ({ buttonText, ...props }: ButtonProp): JSX.Element => {
-  return <button {...props}>{buttonText}</button>;
+const Button = ({ ...props }: ButtonProp): JSX.Element => {
+  return (
+    <button {...props} disabled={props.disabled} type={props.type}>
+      {props.buttonText}
+    </button>
+  );
 };
 
 export default Button;
