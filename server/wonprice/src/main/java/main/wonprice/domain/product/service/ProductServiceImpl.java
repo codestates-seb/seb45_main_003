2f32044ce6964c6984156970314ffae32ce70874
Jeonni <jeonniu@naver.com>
2023-09-01 01:29:19 +0900

    Feat : 상품 등록/삭제 날짜 업로드 로직 추가

--- server/wonprice/src/main/java/main/wonprice/domain/product/service/ProductServiceImpl.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/service/ProductServiceImpl.java
@@ -6,6 +6,7 @@ import main.wonprice.domain.product.entity.Product;
 import main.wonprice.domain.product.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,23 +17,43 @@ public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
 
-    // 상품 등록
+    /*
+        상품 등록
+        - createAt(등록시간): now()
+     */
     @Override
     public Product save(Product product) {
+        product.setCreateAt(LocalDateTime.now());
         return productRepository.save(product);
     }
 
-    // 전체 상품
+    // 전체 상품 조회
     @Override
     public List<Product> findAll() {
         return productRepository.findAll();
     }
 
-    // 특정 상품
+    /*
+        특정 상품 조회
+        - views(조회수): +1 씩 증가
+     */
     @Override
     public Product findOneById(Long productId) {
         Product product = findExistsProduct(productId);
-        product.setViews(product.getViews() + 1); // 조회수 카운팅 (메서드 추가!)
+        product.setViews(product.getViews() + 1);
+        return productRepository.save(product);
+    }
+
+    /*
+        상품 게시글 삭제
+        - removed(삭제여부): false (0) -> true (1)
+        - deletedAt(삭제시간): now()
+     */
+    @Override
+    public Product deleteOneById(Long productId) {
+        Product product = findOneById(productId);
+        product.setDeletedAt(LocalDateTime.now());
+        product.setRemoved(true);
         return productRepository.save(product);
     }
 
@@ -41,12 +62,5 @@ public class ProductServiceImpl implements ProductService {
     public Product findExistsProduct(Long productId) {
         Optional<Product> product = productRepository.findById(productId);
         return product.orElseThrow();
-    }
-
-    @Override
-    public Product deleteOneById(Long productId) {
-        Product product = findOneById(productId);
-        product.setRemoved(true);
-        return productRepository.save(product);
     }
 }
