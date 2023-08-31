201f148d8065f5ef7878a0df31a1886af87c713b
Jeonni <jeonniu@naver.com>
2023-08-31 14:19:56 +0900

    Feat : 상품 등록 POST API 수정

--- server/wonprice/src/main/java/main/wonprice/domain/product/controller/ProductController.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/controller/ProductController.java
@@ -3,21 +3,14 @@ package main.wonprice.domain.product.controller;
 import lombok.RequiredArgsConstructor;
 import main.wonprice.domain.member.entity.Member;
 import main.wonprice.domain.member.service.MemberService;
-import main.wonprice.domain.product.dto.ProductPostDto;
+import main.wonprice.domain.product.dto.ProductRequestDto;
 import main.wonprice.domain.product.dto.ProductResponseDto;
 import main.wonprice.domain.product.entity.Product;
 import main.wonprice.domain.product.mapper.ProductMapper;
 import main.wonprice.domain.product.service.ProductService;
-import main.wonprice.utils.UriCreator;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import javax.validation.constraints.Positive;
-import java.net.URI;
 
 @Controller
 @RequestMapping("/products")
@@ -26,22 +19,14 @@ public class ProductController {
 
     private final ProductService productService;
     private final ProductMapper productMapper;
+    private final MemberService memberService;
 
     // 상품 등록
-    @PostMapping("/register")
-    public ResponseEntity postProduct(@RequestBody ProductPostDto postDto) {
-        Product product = productService.createProduct(productMapper.postDtoToProduct(postDto));
-        URI location = UriCreator.createUri("/products", product.getProductId());
-        return ResponseEntity.created(location).body(product);
-    }
-
-    // 상품 상세 정보 페이지로 이동
-    @GetMapping("/list/{productId}")
-    public ResponseEntity productDetailsPage(@PathVariable("productId") @Positive Long productId) {
-        // 조회할 판매 상품 select
-        Product product = productService.getProduct(productId);
-        ProductResponseDto productResponseDto = productMapper.responseDtoToProduct(product);
-
-        return new ResponseEntity(productResponseDto, HttpStatus.OK);
+    @PostMapping("/post")
+    public ResponseEntity createProduct(@RequestBody ProductRequestDto productRequestDto) {
+        Member loginMember = memberService.findLoginMember();
+        Product product = productService.save(productMapper.toEntity(productRequestDto, loginMember));
+        ProductResponseDto productResponseDto = productMapper.fromEntity(product);
+        return ResponseEntity.ok(productResponseDto);
     }
 }
