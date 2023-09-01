86a8bd72d15a4a47c4f3385991787acc078e6fbd
jungseo <jungwook22@gmail.com>
2023-09-01 14:00:46 +0900

    :sparkles: 마이페이지에 띄울 로그인한 회원이 작성한 글 목록 리턴 로직 추가 및 페이지네이션

--- server/wonprice/src/main/java/main/wonprice/domain/product/controller/ProductController.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/controller/ProductController.java
@@ -3,11 +3,14 @@ package main.wonprice.domain.product.controller;
 import lombok.RequiredArgsConstructor;
 import main.wonprice.domain.member.entity.Member;
 import main.wonprice.domain.member.service.MemberService;
+import main.wonprice.domain.product.dto.ProductMypageResponseDto;
 import main.wonprice.domain.product.dto.ProductRequestDto;
 import main.wonprice.domain.product.dto.ProductResponseDto;
 import main.wonprice.domain.product.entity.Product;
 import main.wonprice.domain.product.mapper.ProductMapper;
 import main.wonprice.domain.product.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -51,5 +54,17 @@ public class ProductController {
         Product product = productService.findOneById(productId);
         ProductResponseDto productResponseDto = productMapper.fromEntity(product);
         return ResponseEntity.ok(productResponseDto);
+    }
+
+    //    마이페이지용 로그인한 회원 게시물 목록 조회
+    @GetMapping("/myPage")
+    public ResponseEntity findLoginMembersProduct(Pageable pageable) {
+
+        Member loginMember = memberService.findLoginMember();
+
+        List<Product> products = productService.findLoginMembersProduct(pageable, loginMember);
+        List<ProductMypageResponseDto> response = productMapper.toMypageProduct(products);
+
+        return ResponseEntity.ok(response);
     }
 }
