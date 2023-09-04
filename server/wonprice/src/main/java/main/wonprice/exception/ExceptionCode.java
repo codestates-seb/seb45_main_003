53470b0077ef7f3d4f3ea53fe35e20e228add325
jungseo <jungwook22@gmail.com>
2023-09-04 13:55:57 +0900

    :sparkles: 찜 물품, 리뷰 찾기 실패 코드 추가

--- server/wonprice/src/main/java/main/wonprice/exception/ExceptionCode.java
+++ server/wonprice/src/main/java/main/wonprice/exception/ExceptionCode.java
@@ -10,7 +10,9 @@ public enum ExceptionCode {
     MEMBER_PHONE_EXISTS(409, "Member Phone exists"),
     MEMBER_NOT_AUTHENTICATED(401, "Member not Authenticated"),
     MEMBER_NOT_AUTHORIZED(403, "Member not Authorized"),
-    INVALID_PASSWORD(403, "Invalid Password");
+    INVALID_PASSWORD(403, "Invalid Password"),
+    WISH_NOT_FOUND(404, "Wish not found"),
+    REVIEW_NOT_FOUND(404, "Review not found");
 
     private int status;
     private String message;
