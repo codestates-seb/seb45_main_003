f1b074fc726d2399e47e5d5efc2ad1721b907506
jungseo <jungwook22@gmail.com>
2023-09-04 13:58:08 +0900

    :sparkles: 유효성 검증 어노테이션 추가

--- server/wonprice/src/main/java/main/wonprice/domain/member/dto/ReviewPostDto.java
+++ server/wonprice/src/main/java/main/wonprice/domain/member/dto/ReviewPostDto.java
@@ -2,12 +2,17 @@ package main.wonprice.domain.member.dto;
 
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 public class ReviewPostDto {
 
+    @NotBlank
     private String content;
 
+    @NotBlank
     private Long score;
 
+    @NotBlank
     private Long targetMemberId;
 }
