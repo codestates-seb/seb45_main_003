46385c8f70bd30d30950148852db5c564b1e01d6
jungseo <jungwook22@gmail.com>
2023-09-04 15:40:27 +0900

    :sparkles: 로그인 유지용 access 토큰 검증 추가

---
+++ server/wonprice/src/main/java/main/wonprice/auth/jwt/controller/JwtController.java
@@ -0,0 +1,39 @@
+package main.wonprice.auth.jwt.controller;
+
+import lombok.AllArgsConstructor;
+import main.wonprice.auth.jwt.JwtTokenizer;
+import main.wonprice.auth.jwt.service.JwtService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@AllArgsConstructor
+public class JwtController {
+
+    private final JwtService jwtService;
+    private final JwtTokenizer tokenizer;
+
+    @GetMapping("/refresh")
+    public ResponseEntity newAccessToken(HttpServletRequest request) {
+
+        String accessToken = jwtService.generateNewAccessToken(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .header("Authorization", "Bearer " + accessToken)
+                .build();
+    }
+
+    @GetMapping("/access")
+    public ResponseEntity verifyTokenExpiration(HttpServletRequest request) {
+
+        jwtService.verifyAccessToken(request);
+        String accessToken = request.getHeader("Authorization");
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
