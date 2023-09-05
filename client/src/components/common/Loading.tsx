30e6f244070dbe1722e7032ddd4eb3739d2c430b
pearlsea <sakanaplanet@gmail.com>
2023-09-05 17:54:53 +0900

    feat: 누락된 커밋, footer 높이 계산 상태 추가

--- client/src/components/common/Loading.tsx
+++ client/src/components/common/Loading.tsx
@@ -1,7 +1,35 @@
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
 import loadingImg from "../../assets/images/loading.gif";
+import { footerHeightState, headerHeightState } from "../../atoms/atoms";
+
+type HeightProps = {
+  $headerheight: number;
+  $footerheight: number;
+};
+
+const StyledLoading = styled.section<HeightProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: calc(
+    100vh - ${(props) => props.$headerheight}px - ${(props) => props.$footerheight}px
+  );
+
+  img {
+    margin: 0 0 ${(props) => props.$headerheight}px;
+  }
+`;
 
 const Loading = (): JSX.Element => {
-  return <img src={loadingImg} alt="loading" />;
+  const headerHeight = useRecoilValue(headerHeightState);
+  const footerHeight = useRecoilValue(footerHeightState);
+
+  return (
+    <StyledLoading $headerheight={headerHeight} $footerheight={footerHeight}>
+      <img src={loadingImg} alt="loading" />
+    </StyledLoading>
+  );
 };
 
 export default Loading;
