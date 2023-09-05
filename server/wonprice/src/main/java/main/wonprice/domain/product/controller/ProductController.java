084d81c90936adfb9f659c0235475226624b5b6e
Jeonni <jeonniu@naver.com>
2023-09-06 01:14:35 +0900

    Feat: 상품 등록 시, 카테고리 정보 추가 로직 구현

--- server/wonprice/src/main/java/main/wonprice/domain/product/controller/ProductController.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/controller/ProductController.java
@@ -1,18 +1,19 @@
 package main.wonprice.domain.product.controller;
 
 import lombok.RequiredArgsConstructor;
+import main.wonprice.domain.category.entity.Category;
+import main.wonprice.domain.category.service.CategoryService;
 import main.wonprice.domain.member.entity.Member;
 import main.wonprice.domain.member.service.MemberService;
 import main.wonprice.domain.product.dto.ProductRequestDto;
 import main.wonprice.domain.product.dto.ProductResponseDto;
 import main.wonprice.domain.product.entity.Product;
 import main.wonprice.domain.product.mapper.ProductMapper;
-import main.wonprice.domain.product.service.ProductService;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import main.wonprice.domain.product.service.ProductService;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -25,12 +26,14 @@ public class ProductController {
     private final ProductService productService;
     private final ProductMapper productMapper;
     private final MemberService memberService;
+    private final CategoryService categoryService;
 
     // 상품 등록
     @PostMapping
     public ResponseEntity createProduct(@RequestBody ProductRequestDto productRequestDto) {
         Member loginMember = memberService.findLoginMember();
-        Product product = productService.save(productMapper.toEntity(productRequestDto, loginMember));
+        Category category = categoryService.findById(productRequestDto.getCategoryId());
+        Product product = productService.save(productMapper.toEntity(productRequestDto, loginMember, category));
         ProductResponseDto productResponseDto = productMapper.fromEntity(product);
         return ResponseEntity.ok(productResponseDto);
     }
