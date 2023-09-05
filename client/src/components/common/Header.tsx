461b63f883c72d5880136e868db028cc7e88343d
dahae <eg1377@naver.com>
2023-09-05 23:24:01 +0900

    🐛 1) 헤더 영역 로그인 / 로그아웃 변경 코드 작성 * 뭔가 충돌이 나는지 에러가 뜸 확인 해봐야함

--- client/src/components/common/Header.tsx
+++ client/src/components/common/Header.tsx
@@ -1,3 +1,5 @@
+// 새로 합친 코드
+
 // import React, { useEffect, useRef } from "react";
 // import { useRecoilValue, useSetRecoilState } from "recoil";
 // import { headerHeightState, userState } from "../../atoms/atoms";
@@ -22,19 +24,46 @@
 
 // export default MainHeader;
 
-import { useRecoilValue } from "recoil";
-import { loginState } from "../../atoms/atoms";
+// 기존 코드
+// import { useRecoilValue } from "recoil";
+// import { loginState } from "../../atoms/atoms";
+// import HeaderLogin from "../header/HeaderLogin"; // 로그인 시 표시할 헤더
+// import HeaderLogout from "../header/HeaderLogout"; // 로그아웃 시 표시할 헤더
+// import { useValidateToken } from "../../hooks/useValidateToken";
+
+// const MainHeader: React.FC = () => {
+//   const user = useRecoilValue(loginState);
+//   useValidateToken();
+
+//   console.log(user);
+
+//   return <>{user ? <HeaderLogin /> : <HeaderLogout />}</>;
+// };
+
+// export default MainHeader;
+
+//수정 코드
+import React, { useEffect, useRef } from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { headerHeightState, loginState } from "../../atoms/atoms"; // loginState 추가
 import HeaderLogin from "../header/HeaderLogin"; // 로그인 시 표시할 헤더
 import HeaderLogout from "../header/HeaderLogout"; // 로그아웃 시 표시할 헤더
-import { useValidateToken } from "../../hooks/useValidateToken";
 
 const MainHeader: React.FC = () => {
-  const user = useRecoilValue(loginState);
-  useValidateToken();
+  // 헤더 높이 구하기
+  const headerRef = useRef<HTMLDivElement | null>(null);
+  const setHeaderHeight = useSetRecoilState(headerHeightState);
 
-  console.log(user);
+  useEffect(() => {
+    if (headerRef.current !== null) {
+      setHeaderHeight(headerRef.current?.clientHeight);
+    }
+  }, [setHeaderHeight]);
 
-  return <>{user ? <HeaderLogin /> : <HeaderLogout />}</>;
+  // 로그인 상태 확인
+  const isLoggedIn = useRecoilValue(loginState); // loginState 사용
+
+  return <div ref={headerRef}>{isLoggedIn ? <HeaderLogin /> : <HeaderLogout />}</div>;
 };
 
 export default MainHeader;
