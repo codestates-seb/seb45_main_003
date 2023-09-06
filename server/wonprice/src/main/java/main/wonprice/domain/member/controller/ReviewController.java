6ea02406c64167a8755a8f16d20c42243790fcf4
jungseo <jungwook22@gmail.com>
2023-09-06 15:40:06 +0900

    :recycle: member-id pathVariable 추가

--- server/wonprice/src/main/java/main/wonprice/domain/member/controller/ReviewController.java
+++ server/wonprice/src/main/java/main/wonprice/domain/member/controller/ReviewController.java
@@ -9,7 +9,6 @@ import main.wonprice.domain.member.entity.Review;
 import main.wonprice.domain.member.mapper.ReviewMapper;
 import main.wonprice.domain.member.service.MemberService;
 import main.wonprice.domain.member.service.ReviewService;
-import main.wonprice.domain.product.service.ProductService;
 import main.wonprice.domain.product.service.ProductServiceImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -40,24 +39,13 @@ public class ReviewController {
         return new ResponseEntity(response, HttpStatus.CREATED);
     }
 
-    @GetMapping("/members/my-page/reviews/wrote")
-    public ResponseEntity fingLoginMemberwroteReview(Pageable pageable) {
+    @GetMapping("/members/{member-id}/reviews/post")
+    public ResponseEntity fingMemberwroteReview(Pageable pageable,
+                                                     @PathVariable("member-id")Long memberId) {
 
-        Member loginMember = memberService.findLoginMember();
+        Member member = memberService.findMember(memberId);
 
-        List<Review> reviews = reviewService.findWroteReviews(pageable, loginMember);
-        List<ReviewResponseDto> response = mapper.reviewsToResponseDtos(reviews);
-
-        return new ResponseEntity(response, HttpStatus.OK);
-    }
-
-
-    @GetMapping("/members/my-page/reviews")
-    public ResponseEntity findLoginMembersReview(Pageable pageable) {
-
-        Member loginMember = memberService.findLoginMember();
-
-        List<Review> reviews = reviewService.findReviews(pageable, loginMember);
+        List<Review> reviews = reviewService.findWroteReviews(pageable, member);
         List<ReviewResponseDto> response = mapper.reviewsToResponseDtos(reviews);
 
         return new ResponseEntity(response, HttpStatus.OK);
