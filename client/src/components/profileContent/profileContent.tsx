6fdff9855a82ab2076598d12e18e16fe71603e61
sintobury <herkongho@gmail.com>
2023-09-06 14:52:24 +0900

    password modify modal 구현

--- client/src/components/profileContent/profileContent.tsx
+++ client/src/components/profileContent/profileContent.tsx
@@ -5,13 +5,20 @@ import Button from "../common/Button";
 import PostListTab from "./postListTab";
 import { useEffect, useState } from "react";
 import axios from "axios";
-// import { useModal } from "../../hooks/useModal";
+import { useModal } from "../../hooks/useModal";
+import Modal from "../common/Modal";
+import { useForm } from "react-hook-form";
 
 interface Profile {
   memberId: number;
   name: string;
   email: string;
   phone: string;
+}
+
+interface modifyPasswordForm {
+  passwordCheck: string;
+  newPassword: string;
 }
 
 const ProfileContentContainer = styled.div`
@@ -67,11 +74,56 @@ const ProfileContentContainer = styled.div`
     }
   }
 `;
+const StyledModal = styled.form`
+  width: 25rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: stretch;
+  gap: 16px;
+  .modalInputContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: stretch;
+    gap: 0.5rem;
+    .ButtonContainer {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      align-items: flex-end;
+      gap: 1rem;
+    }
+    .errormessage {
+      color: ${COLOR.invalid};
+    }
+    .successmessage {
+      color: ${COLOR.valid};
+    }
+    .errorInput {
+      border-color: ${COLOR.invalid};
+    }
+  }
+  .modalButtonContainer {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+`;
 
 const ProfileContent = (): JSX.Element => {
   const [profile, setProfile] = useState<Profile>({ memberId: 0, name: "", email: "", phone: "" });
   // const Id = window.location.search
-  // const { setIsOpen, closeModal, isOpen } = useModal();
+  const [pass, setPass] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    setError,
+    getValues,
+  } = useForm<modifyPasswordForm>();
+  const { toggleModal, closeModal, isOpen } = useModal();
   const getProfile = async () => {
     const res = await axios.get(`${process.env.REACT_APP_API_URL}/members/myPage`, {
       headers: {
@@ -80,7 +132,43 @@ const ProfileContent = (): JSX.Element => {
     });
     setProfile(res.data);
   };
-
+  const modifyPassword = async (body: modifyPasswordForm) => {
+    const res = await axios.patch(`${process.env.REACT_APP_API_URL}/members/myPage`, body, {
+      headers: {
+        Authorization: localStorage.getItem("accessToken"),
+      },
+    });
+    setProfile(res.data);
+  };
+  const validatePassword = async (body: string) => {
+    try {
+      const res = await axios.post(
+        `${process.env.REACT_APP_API_URL}/members/auth/password`,
+        { password: body },
+        {
+          headers: {
+            Authorization: localStorage.getItem("accessToken"),
+          },
+        },
+      );
+      if (res.status === 200) {
+        setPass(true);
+      }
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        if (error.response?.status === 403) {
+          setPass(false);
+          setError("passwordCheck", {
+            message: "틀린 비밀번호입니다.",
+          });
+        }
+      }
+    }
+  };
+  const resetModal = () => {
+    setPass(false);
+    toggleModal();
+  };
   useEffect(() => {
     getProfile();
   }, []);
@@ -89,7 +177,12 @@ const ProfileContent = (): JSX.Element => {
       <ProfileContentContainer>
         <div className="topContainer">
           <p className="menuTitle">프로필</p>
-          <Button type="button" $text="비밀번호 변경" $design="black" />
+          <Button
+            type="button"
+            $text="비밀번호 변경"
+            onClick={() => toggleModal()}
+            $design="black"
+          />
         </div>
         <div className="profileInfoContainer">
           <img className="profileImg"></img>
@@ -107,6 +200,51 @@ const ProfileContent = (): JSX.Element => {
           </ul>
         </div>
         <PostListTab />
+        <Modal isOpen={isOpen} closeModal={closeModal} toggleModal={resetModal}>
+          <StyledModal onSubmit={handleSubmit(modifyPassword)}>
+            <div className="modalInputContainer">
+              <label htmlFor="passwordCheck">비밀번호</label>
+              <input
+                id="passwordCheck"
+                type="password"
+                placeholder="기존 비밀번호를 확인해야 변경 가능합니다."
+                className={errors.passwordCheck && !pass ? "errorInput" : "input"}
+                {...register("passwordCheck", {
+                  required: "현재 비밀번호를 입력해주세요.",
+                })}
+              ></input>
+              <div className="ButtonContainer">
+                {errors.passwordCheck && !pass && (
+                  <div className="errormessage">{errors.passwordCheck?.message}</div>
+                )}
+                {pass && <div className="successmessage">확인되었습니다.</div>}
+                <Button
+                  type="button"
+                  $text="비밀번호 확인"
+                  $design="black"
+                  onClick={() => validatePassword(getValues("passwordCheck"))}
+                />
+              </div>
+              <label htmlFor="newPassword">새 비밀번호</label>
+              <input
+                id="newPassword"
+                type="password"
+                placeholder="새 비밀번호"
+                disabled={!pass}
+                className={errors.newPassword ? "errorInput" : "input"}
+                {...register("newPassword", {
+                  required: "새로운 비밀번호를 입력해주세요.",
+                })}
+              ></input>
+              {errors.newPassword && (
+                <div className="errormessage">{errors.newPassword?.message}</div>
+              )}
+            </div>
+            <div className="modalButtonContainer">
+              <Button type="submit" $text="확인" $design="black" disabled={isSubmitting} />
+            </div>
+          </StyledModal>
+        </Modal>
       </ProfileContentContainer>
     );
   } else {
