872166501e54feceeb3454fe19ed6dee96d2fa4c
dahae <eg1377@naver.com>
2023-09-07 16:07:17 +0900

    🐛 시간 1시간 으로 늘림

--- client/src/components/chatting/MessageBubble.tsx
+++ client/src/components/chatting/MessageBubble.tsx
@@ -60,51 +60,26 @@ const ChatRoom: React.FC = () => {
   // 문자열을 숫자로 변환합니다. 로컬 스토리지에 값이 없으면 null로 설정합니다.
   const Id = userIdFromLocalStorage ? parseInt(userIdFromLocalStorage, 10) : null;
 
-  // useEffect(() => {
-  //   const fetchMessages = async () => {
-  //     try {
-  //       const response = await axios.get(`${process.env.REACT_APP_API_URL}/chat/${chatRoomId}`);
-  //       console.log(fetchMessages);
-
-  //       setMessages(response.data);
-  //     } catch (error) {
-  //       console.error("Failed to fetch messages:", error);
-  //     }
-  //   };
-
-  //   fetchMessages();
-  //   // const intervalId = setInterval(fetchMessages, 5000); // 5초마다 메시지를 새로 가져옵니다.
-  //   const intervalId = setInterval(fetchMessages, 3600000); // 1시간마다 메시지를 새로 가져옵니다.
-
-  //   return () => {
-  //     clearInterval(intervalId); // 컴포넌트가 언마운트되면 인터벌을 제거합니다.
-  //   };
-  // }, [chatRoomId]); // chatRoomId를 의존성으로 추가
-
   useEffect(() => {
-    const fetchMessagesWithLongPolling = async () => {
+    const fetchMessages = async () => {
       try {
-        // 서버가 준비된 데이터를 보낼 때까지 대기
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/chat/${chatRoomId}`, {
-          timeout: 50000, // 타임아웃을 길게 설정
-        });
-        setMessages(response.data);
+        const response = await axios.get(`${process.env.REACT_APP_API_URL}/chat/${chatRoomId}`);
+        console.log(fetchMessages);
 
-        // 데이터를 받은 후 다음 Long Polling 요청을 바로 보냄
-        fetchMessagesWithLongPolling();
+        setMessages(response.data);
       } catch (error) {
-        console.error("An error occurred:", error);
-        // 에러 발생 시 다시 Long Polling 시작
-        fetchMessagesWithLongPolling();
+        console.error("Failed to fetch messages:", error);
       }
     };
 
-    fetchMessagesWithLongPolling(); // 최초의 Long Polling 요청
+    fetchMessages();
+    // const intervalId = setInterval(fetchMessages, 50000); // 5초마다 메시지를 새로 가져옵니다.
+    const intervalId = setInterval(fetchMessages, 3600000); // 1시간마다 메시지를 새로 가져옵니다.
 
     return () => {
-      // 컴포넌트가 언마운트되면 여기에 정리 로직을 작성
+      clearInterval(intervalId); // 컴포넌트가 언마운트되면 인터벌을 제거합니다.
     };
-  }, [chatRoomId]); // chatRoomId가 변경될 때마다 Long Polling을 재시작
+  }, [chatRoomId]); // chatRoomId를 의존성으로 추가
 
   return (
     <div>
@@ -125,5 +100,3 @@ const ChatRoom: React.FC = () => {
 };
 
 export default ChatRoom;
-
-// localStorage.getItem("refreshToken")
