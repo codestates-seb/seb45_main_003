8378a23dc2f05fef6193b006fdeca00288340bf7
jungseo <jungwook22@gmail.com>
2023-09-07 18:32:01 +0900

    :sparkles: 구매자, 판매자 상호 리뷰 기능 구현

--- server/wonprice/src/main/java/main/wonprice/domain/member/repository/ReviewRepository.java
+++ server/wonprice/src/main/java/main/wonprice/domain/member/repository/ReviewRepository.java
@@ -2,18 +2,14 @@ package main.wonprice.domain.member.repository;
 
 import main.wonprice.domain.member.entity.Member;
 import main.wonprice.domain.member.entity.Review;
-import main.wonprice.domain.product.entity.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findAllByPostMember(Pageable pageable, Member member);
 
-    Page<Review> findAllByProductSeller(Pageable pageable, Member member);
-
-    Optional<Review> findByPostMemberAndProduct(Member postMember, Product product);
+    Page<Review> findAllByTargetMemberId(Pageable pageable, Long memberId);
 }
