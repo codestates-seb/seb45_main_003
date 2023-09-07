0b019cc0dc14fcd76ad9c37b362642dce1851d25
dahae <eg1377@naver.com>
2023-09-07 18:20:17 +0900

    🐛 웹소켓 연결 확인!

--- client/src/components/chatting/ChatRoom.tsx
+++ client/src/components/chatting/ChatRoom.tsx
@@ -1,57 +1,3 @@
-// import { styled } from "styled-components";
-// import ChatInput from "./ChatInput";
-// import MessageBubble from "./MessageBubble";
-
-// const Container = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   width: 90%; // 상대적인 단위로 변경
-//   height: 43.6875rem;
-//   padding: 1.5rem 1rem;
-
-//   justify-content: end;
-
-//   border-radius: 0.375rem;
-//   border: 1px solid #e0e0e0;
-//   background: #f7f7f7;
-//   /* background-color: aqua; */
-
-//   .chatBox {
-//     overflow-y: auto; /* 내용이 넘칠 경우 스크롤 표시 */
-
-//     &::-webkit-scrollbar {
-//       width: 0px;
-//     }
-//     /* &::-webkit-scrollbar-thumb {
-//       border-radius: 6px;
-//       background: #ccc;
-//     } */
-//   }
-//   @media (max-width: 64rem) {
-//     width: 95%; // 상대적인 단위로 변경
-//   }
-// `;
-
-// const ChatRoom = (): JSX.Element => {
-//   // 사용자가 메시지를 보내면 호출되는 함수
-//   const handleSendMessage = (message: string) => {
-//     console.log("User sent a message:", message);
-//     // 이곳에서 실제로 메시지를 보내는 로직을 구현할 수 있습니다.
-//   };
-
-//   return (
-//     <>
-//       <Container>
-//         <div className="chatBox" style={{ display: "flex", flexDirection: "column" }}>
-//           <MessageBubble />
-//         </div>
-//         <ChatInput onSendMessage={handleSendMessage} />
-//       </Container>
-//     </>
-//   );
-// };
-// export default ChatRoom;
-
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ChatInput from "./ChatInput";
@@ -105,7 +51,7 @@ const ChatRoom = () => {
         {},
         () => {
           console.log("Connected to the WebSocket server");
-          stompClient.subscribe(`/topic/chat/${chatRoomId}`, (message) => {
+          stompClient!.subscribe(`/topic/chat/${chatRoomId}`, (message) => {
             console.log(`Received message: ${message.body}`);
             // 여기에 메시지를 받았을 때의 로직을 추가
           });
@@ -116,6 +62,14 @@ const ChatRoom = () => {
       );
 
       setClient(stompClient);
+      // 정리 함수
+      return () => {
+        if (stompClient && stompClient.connected) {
+          stompClient.disconnect(() => {
+            console.log("Disconnected from the WebSocket server");
+          });
+        }
+      };
     }
   }, [chatRoomId]); // currentChatRoomId가 변경될 때마다 웹소켓을 다시 연결
 
