3d1f0a5e231140a439142bfdb26ff6e49a4b6cec
Jeonni <jeonniu@naver.com>
2023-09-11 00:29:20 +0900

    Feat: ProductMapper에 작성한 리뷰, 받은 리뷰 필드 추가

--- server/wonprice/src/main/java/main/wonprice/domain/product/mapper/ProductMapper.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/mapper/ProductMapper.java
@@ -24,7 +24,7 @@ public interface ProductMapper {
                 .auction(productRequestDto.getAuction())
                 .status(ProductStatus.BEFORE)
                 .views(0L)
-                .wishCount(0)
+                .wishCount(0L)
                 .buyerReview(false)
                 .sellerReview(false);
 
@@ -58,7 +58,9 @@ public interface ProductMapper {
                 .images(product.getProductPictures())
                 .sellerName(product.getSeller().getName()) // 판매자 이름 설정
                 .sellerReputation(product.getSeller().getReputation()) // 판매자 평판 설정
-                .wishCount(product.getWishCount())
+                .writtenReviewsCount(product.getSeller().getWrittenReviewsCount()) // 판매자가 쓴 리뷰 갯수
+                .receivedReviewsCount(product.getSeller().getReceivedReviewsCount()) // 판매자가 받은 리뷰 갯수
+                .wishCount(product.getWishCount()) // 상품의 찜 갯수
 //                .images(product.getProductPictures())
                 .build();
     }
