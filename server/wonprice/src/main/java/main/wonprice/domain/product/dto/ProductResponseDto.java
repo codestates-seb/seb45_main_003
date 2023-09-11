9273bed604c1b37ba44d041a31ddf53c8cd53a96
jungseo <jungwook22@gmail.com>
2023-09-11 23:54:44 +0900

    :sparkles: 상세페이지용 응답 DTO 추가(찜 여부 추가)

--- server/wonprice/src/main/java/main/wonprice/domain/product/dto/ProductResponseDto.java
+++ server/wonprice/src/main/java/main/wonprice/domain/product/dto/ProductResponseDto.java
@@ -59,4 +59,6 @@ public class ProductResponseDto {
     private Long sellerReceivedReviewsCount; // 판매자가 받은 리뷰 개수
 
     private Long wishCount; // 상품이 받은 총 wish 갯수
+
+    private Boolean loginMembersWish; // 로그인한 회원이 해당 상품을 찜 했는지 여부 / 추후에 DTO 분리할게요
 }
