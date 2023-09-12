fa87700ce8d6fd28569c38cc6ace247501d9e2ee
pearlsea <sakanaplanet@gmail.com>
2023-09-12 17:49:12 +0900

    feat: 반응형 헤더 추가

---
+++ client/src/components/header/MobileSidebar.tsx
@@ -0,0 +1,120 @@
+import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
+import CloseIcon from "@mui/icons-material/Close";
+import PersonIcon from "@mui/icons-material/Person";
+import { Link } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import styled from "styled-components";
+import { loginState } from "../../atoms/atoms";
+import { CATEGORY } from "../../constants/category";
+import { COLOR } from "../../constants/color";
+import { FONT_SIZE } from "../../constants/font";
+import { getUserId } from "../../util/auth";
+
+type MobileSidebarProps = {
+  onClose: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const StyledMobileSidebar = styled.div`
+  z-index: 99;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding: 0 1rem;
+  background: ${COLOR.darkText};
+  color: ${COLOR.lightText};
+  box-sizing: border-box;
+
+  .close_icon {
+    margin: 1rem 0 0 auto;
+    display: flex;
+
+    path {
+      fill: #fff;
+    }
+  }
+
+  .user_status {
+    margin: 1.5rem 0 1rem;
+    padding: 0 0 1rem;
+    display: flex;
+    flex-flow: row;
+    align-items: center;
+    gap: 0.25rem;
+    border-bottom: 1px solid rgba(66, 66, 66, 5);
+  }
+
+  .category_title {
+    color: #fff;
+    font-size: ${FONT_SIZE.font_18};
+    font-weight: 700;
+    margin: 0 0 0.75rem;
+  }
+
+  ul {
+    border-bottom: 1px solid rgba(66, 66, 66, 5);
+    padding: 0 0 0.5rem 0;
+
+    li {
+      padding: 0.5rem 0;
+    }
+  }
+`;
+
+const MobileSidebar = ({ onClose }: MobileSidebarProps) => {
+  const isLogin = useRecoilValue(loginState);
+  const userId = getUserId();
+
+  const handleClose = () => {
+    onClose(false);
+  };
+
+  return (
+    <StyledMobileSidebar>
+      <CloseIcon
+        className="close_icon"
+        onClick={() => {
+          onClose(false);
+        }}
+      />
+      <div className="user_status">
+        {isLogin ? (
+          <>
+            <PersonIcon />
+            <Link to={`/member/${userId}`} onClick={handleClose}>
+              프로필
+            </Link>
+            <ChatBubbleIcon />
+            <Link to={`/chat/${userId}`} onClick={handleClose}>
+              채팅
+            </Link>
+          </>
+        ) : (
+          <Link to="/login" onClick={handleClose}>
+            로그인
+          </Link>
+        )}
+      </div>
+      <p className="category_title">Product Category</p>
+      <ul>
+        {Object.keys(CATEGORY).map((category) => {
+          return (
+            <li key={category}>
+              <Link to={CATEGORY[category].path} onClick={handleClose}>
+                {category}
+              </Link>
+            </li>
+          );
+        })}
+        <li key="all">
+          <Link to={`/product`} onClick={handleClose}>
+            View All
+          </Link>
+        </li>
+      </ul>
+    </StyledMobileSidebar>
+  );
+};
+
+export default MobileSidebar;
