b4ed0382cb26581a67527c9dd2a0d6a863214d20
jungseo <jungwook22@gmail.com>
2023-09-12 18:54:19 +0900

    :art: 페이지네이션 방식 변경

--- server/wonprice/src/main/java/main/wonprice/domain/member/controller/MemberController.java
+++ server/wonprice/src/main/java/main/wonprice/domain/member/controller/MemberController.java
@@ -11,6 +11,8 @@ import main.wonprice.domain.product.dto.ProductResponseDto;
 import main.wonprice.domain.product.entity.Product;
 import main.wonprice.domain.product.mapper.ProductMapper;
 import main.wonprice.domain.product.service.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -61,38 +63,47 @@ public class MemberController {
 
         Member member = memberService.findMember(memberId);
 
-        List<Product> products = productService.findMembersProduct(pageable, member);
-        List<ProductResponseDto> response = productMapper.toMypageProduct(products);
+        Page<Product> products = productService.findMembersProduct(pageable, member);
+        Page<ProductResponseDto> response = products.map(productMapper::fromEntity);
 
         return ResponseEntity.ok(response);
     }
 
 //    판매 완료 목록 조회
     @GetMapping("/{member-id}/sell")
-    public ResponseEntity findMembersSell(Pageable pageable,
-                                              @PathVariable("member-id") Long memberId) {
+    public ResponseEntity findMembersSell(@RequestParam(defaultValue = "0") int page,
+                                          @RequestParam(defaultValue = "10") int size,
+                                          @PathVariable("member-id") Long memberId) {
+
+        Pageable pageable = PageRequest.of(page, size);
 
         Member member = memberService.findMember(memberId);
-        List<Product> products = productService.findMemberSold(pageable, member);
-        List<ProductResponseDto> response = productMapper.toMypageProduct(products);
+        Page<Product> products = productService.findMemberSold(pageable, member);
+        Page<ProductResponseDto> response = products.map(productMapper::fromEntity);
 
         return ResponseEntity.ok(response);
     }
 
 //    구매 완료 목록 조회
     @GetMapping("/{member-id}/purchase")
-    public ResponseEntity findMembersPurchase(Pageable pageable,
+    public ResponseEntity findMembersPurchase(@RequestParam(defaultValue = "0") int page,
+                                              @RequestParam(defaultValue = "10") int size,
                                               @PathVariable("member-id") Long memberId) {
 
-        List<Product> products = productService.findMemberBought(pageable, memberId);
-        List<ProductResponseDto> response = productMapper.toMypageProduct(products);
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Product> products = productService.findMemberBought(pageable, memberId);
+        Page<ProductResponseDto> response = products.map(productMapper::fromEntity);
 
         return ResponseEntity.ok(response);
     }
 
 //    회원 목록 조회
     @GetMapping("/all")
-    public ResponseEntity getMembers(Pageable pageable) {
+    public ResponseEntity getMembers(@RequestParam(defaultValue = "0") int page,
+                                     @RequestParam(defaultValue = "10") int size) {
+
+        Pageable pageable = PageRequest.of(page, size);
 
         List<Member> members = memberService.findMembers(pageable);
         List<MemberResponseDto> response = mapper.membersToResponseDtos(members);
