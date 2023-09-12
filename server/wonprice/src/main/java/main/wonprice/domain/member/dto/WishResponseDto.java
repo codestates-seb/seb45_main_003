42f195abb939962e5e09529b0e78974bc6bee6e6
jungseo <jungwook22@gmail.com>
2023-09-12 18:59:28 +0900

    :sparkles: 생성자 추가

--- server/wonprice/src/main/java/main/wonprice/domain/member/dto/WishResponseDto.java
+++ server/wonprice/src/main/java/main/wonprice/domain/member/dto/WishResponseDto.java
@@ -1,12 +1,14 @@
 package main.wonprice.domain.member.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import main.wonprice.domain.product.dto.ProductResponseDto;
 
 import java.time.LocalDateTime;
 
 @Getter @Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class WishResponseDto {
 
     private Long wishId;
