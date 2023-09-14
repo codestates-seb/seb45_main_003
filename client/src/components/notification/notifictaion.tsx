e34a3ffc1ee3369f81336e152f1698038ad2e658
sintobury <herkongho@gmail.com>
2023-09-14 21:02:31 +0900

    login 해결,logout구현, 알림창 모달화 구현

--- client/src/components/notification/notifictaion.tsx
+++ client/src/components/notification/notifictaion.tsx
@@ -3,8 +3,8 @@ import { styled } from "styled-components";
 import { COLOR } from "../../constants/color";
 import { FONT_SIZE } from "../../constants/font";
 import { authInstance } from "../../interceptors/interceptors";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 // import ErrorIndication from "../../pages/ErrorIndication";
 import Loading from "../common/Loading";
 import { useRecoilState } from "recoil";
@@ -13,6 +13,7 @@ import { postListTabState } from "../../atoms/atoms";
 import { findCategory } from "../../util/category";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import { getUnReadCount, getNotifications } from "./notificationFunction";
+import { useRef } from "react";
 
 interface notificationData {
   content: notification[];
@@ -29,53 +30,55 @@ interface notification {
   categoryId: number | null;
 }
 
-const NotificationIcon = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: stretch;
-  position: relative;
-  .iconContainer {
-    display: flex;
-    flex-direction: row;
-    align-items: flex-end;
-    .readCount {
-      font-size: ${FONT_SIZE.font_20};
-      font-weight: bold;
-      color: ${COLOR.darkText};
-    }
-  }
-  .notificationListContainer {
-    position: absolute;
-    top: 40px;
-    left: -200px;
-    z-index: 99;
-    border: 1px solid ${COLOR.border};
-    background-color: ${COLOR.gray_100};
-    border-radius: 6px;
-    width: 18.75rem;
-    height: 263px;
+const Notification = styled.div`
+  .icon {
     display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    justify-content: center;
     align-items: stretch;
-    font-size: ${FONT_SIZE.font_16};
-    overflow: scroll;
-    .notification {
+    position: relative;
+    .iconContainer {
       display: flex;
       flex-direction: row;
-      justify-content: space-between;
-      align-items: center;
-      border-bottom: 1px solid ${COLOR.border};
-      padding: 1rem;
-      &:last-child {
-        border-bottom: none;
-      }
-      &.isRead {
-        color: ${COLOR.mediumText};
-      }
-      &.notRead {
+      align-items: flex-end;
+      .readCount {
+        font-size: ${FONT_SIZE.font_20};
+        font-weight: bold;
         color: ${COLOR.darkText};
+      }
+    }
+    .notificationListContainer {
+      position: absolute;
+      top: 40px;
+      left: -200px;
+      z-index: 99;
+      border: 1px solid ${COLOR.border};
+      background-color: ${COLOR.gray_100};
+      border-radius: 6px;
+      width: 18.75rem;
+      height: 263px;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: stretch;
+      font-size: ${FONT_SIZE.font_16};
+      overflow: scroll;
+      .notification {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        border-bottom: 1px solid ${COLOR.border};
+        padding: 1rem;
+        &:last-child {
+          border-bottom: none;
+        }
+        &.isRead {
+          color: ${COLOR.mediumText};
+        }
+        &.notRead {
+          color: ${COLOR.darkText};
+        }
       }
     }
   }
@@ -87,6 +90,7 @@ const Notifications = (): JSX.Element => {
   const [menu, setMenu] = useRecoilState(postListTabState);
   const ID = localStorage.getItem("Id");
   const navigate = useNavigate();
+  const location = useLocation();
   const queryClient = useQueryClient();
   const openNotification = () => {
     setOpen(!open);
@@ -100,7 +104,7 @@ const Notifications = (): JSX.Element => {
   );
   const notificationMutation = useMutation(
     async (notification: notification) => {
-      await authInstance.patch(`/notification/${notification.notificationId}`);
+      await authInstance.patch(`/notifications/${notification.notificationId}`);
     },
     {
       onSuccess: () => queryClient.invalidateQueries("notification"),
@@ -120,33 +124,49 @@ const Notifications = (): JSX.Element => {
     }
     notificationMutation.mutateAsync(notification);
   };
-  const getReadCount = useQuery(["readCount"], getUnReadCount);
+  const notificationRef = useRef<HTMLDivElement>(null);
+  const getReadCount = useQuery(["readCount", { location }], getUnReadCount);
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent): void => {
+      if (notificationRef.current && !notificationRef.current.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [notificationRef]);
   return (
-    <NotificationIcon onClick={openNotification}>
-      <div className="iconContainer">
-        {getReadCount.isLoading ? null : (
-          <div className="readCount">{getReadCount.data.unreadNotification}</div>
-        )}
-        <NotificationsIcon className="noticiationIcon" />
-      </div>
-      {open && (
-        <ul className="notificationListContainer">
-          {isLoading ? (
-            <Loading />
-          ) : (
-            notificationInfo?.content.map((el, idx) => (
-              <li
-                className={el.isRead ? "isRead notification" : "notRead notification"}
-                key={idx}
-                onClick={() => navigatePost(el)}
-              >
-                <div>{el.content}</div>
-              </li>
-            ))
+    <Notification ref={notificationRef}>
+      <div className="icon">
+        <div className="iconContainer" onClick={openNotification}>
+          {getReadCount.isLoading ? null : (
+            <div className="readCount">
+              {getReadCount.data.unreadNotification !== 0 && getReadCount.data.unreadNotification}
+            </div>
           )}
-        </ul>
-      )}
-    </NotificationIcon>
+          <NotificationsIcon className="noticiationIcon" />
+        </div>
+        {open && (
+          <ul className="notificationListContainer" onBlur={() => setOpen(!open)}>
+            {isLoading ? (
+              <Loading />
+            ) : (
+              notificationInfo?.content.map((el, idx) => (
+                <li
+                  className={el.isRead ? "isRead notification" : "notRead notification"}
+                  key={idx}
+                  onClick={() => navigatePost(el)}
+                >
+                  <div>{el.content}</div>
+                </li>
+              ))
+            )}
+          </ul>
+        )}
+      </div>
+    </Notification>
   );
 };
 
