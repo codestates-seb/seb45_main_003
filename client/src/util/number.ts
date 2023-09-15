dd3a643ab54faaa529796d0eeb90f0e2feb7a70d
pearlsea <sakanaplanet@gmail.com>
2023-09-15 18:14:17 +0900

    feat: 5% 더한 값 계산해주는 유틸 함수 추가

--- client/src/util/number.ts
+++ client/src/util/number.ts
@@ -1,3 +1,7 @@
 export const allowOnlyNumber = (event: React.ChangeEvent<HTMLInputElement>) => {
   event.target.value = event.target.value.replace(/[^0-9]/g, "");
 };
+
+export const plus5Percent = (number: number) => {
+  return number + Math.ceil((number * 0.05) / 10) * 10;
+};
