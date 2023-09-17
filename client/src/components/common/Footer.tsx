cfc802147e5903f4f7c891923cfab0a8da6afc5a
sintobury <herkongho@gmail.com>
2023-09-17 16:31:18 +0900

    footer 팀원 깃허브링크 추가

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { footerHeightState } from "../../atoms/atoms";
+import { FONT_SIZE } from "../../constants/font";
 
 const FooterStyled = styled.div`
   justify-content: center;
@@ -20,6 +21,23 @@ const FooterStyled = styled.div`
 
   .fotterText {
     margin-left: 5rem;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    .partTitle {
+      font-size: ${FONT_SIZE.font_20};
+      font-weight: bold;
+    }
+    .teammateContainer {
+      display: flex;
+      flex-direction: row;
+      .teammateLink {
+        font-size: ${FONT_SIZE.font_20};
+        padding: 0.5rem;
+      }
+    }
   }
 `;
 
@@ -38,7 +56,33 @@ export const Footer = (): JSX.Element => {
     <>
       <FooterStyled ref={footerRef}>
         <footer>
-          <div className="fotterText">CompanyName @ 202X. All rights reserved.</div>
+          <div className="fotterText">
+            <p className="partTitle">팀원 깃허브 링크</p>
+            <p className="partTitle">BE</p>
+            <div className="teammateContainer">
+              <a className="teammateLink" href="https://github.com/C5ng">
+                공대표
+              </a>
+              <a className="teammateLink" href="https://github.com/Jeonni">
+                이지연
+              </a>
+              <a className="teammateLink" href="https://github.com/Jung-seo">
+                서정욱
+              </a>
+            </div>
+            <p className="partTitle">FE</p>
+            <div className="teammateContainer">
+              <a className="teammateLink" href="https://github.com/sintobury">
+                허찬욱
+              </a>
+              <a className="teammateLink" href="https://github.com/eg1377">
+                박다해
+              </a>
+              <a className="teammateLink" href="https://github.com/pearl-sea">
+                박진주
+              </a>
+            </div>
+          </div>
         </footer>
       </FooterStyled>
     </>
