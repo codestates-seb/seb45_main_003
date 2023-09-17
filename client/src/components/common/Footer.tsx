33bae53e9315996514b9fccbffae2e09853fad2a
pearlsea <sakanaplanet@gmail.com>
2023-09-17 18:24:52 +0900

    design: 푸터 디자인 변경

--- client/src/components/common/Footer.tsx
+++ client/src/components/common/Footer.tsx
@@ -2,41 +2,71 @@ import { useEffect, useRef } from "react";
 import { useSetRecoilState } from "recoil";
 import styled from "styled-components";
 import { footerHeightState } from "../../atoms/atoms";
+import { COLOR } from "../../constants/color";
 import { FONT_SIZE } from "../../constants/font";
 
-const FooterStyled = styled.div`
-  justify-content: center;
-  align-items: center;
-  color: var(--Text, #212121);
+const FooterStyled = styled.footer`
+  background: ${COLOR.gray_800};
+  color: ${COLOR.lightText};
   padding: 1.5rem 0;
 
-  /* Body/S */
-  font-family: Roboto;
-  font-size: 0.875rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 140%;
-
-  background: #ffcd57;
-
-  .fotterText {
-    margin-left: 5rem;
+  .footer_wrapper,
+  .copyright {
+    max-width: 90rem;
+    width: calc(100% - 3rem);
+    margin: 0 auto;
     display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    gap: 2rem;
-    .partTitle {
-      font-size: ${FONT_SIZE.font_20};
-      font-weight: bold;
+    flex-flow: row;
+    gap: 1.5rem;
+  }
+
+  .copyright {
+    margin: 1.5rem auto 0;
+    border-top: 1px solid ${COLOR.gray_600};
+    padding: 1rem 0 0;
+  }
+
+  .part {
+    width: 20%;
+    display: flex;
+    flex-flow: column;
+
+    .title {
+      font-size: ${FONT_SIZE.font_18};
+      font-weight: 700;
+      margin: 0 0 0.75rem;
     }
-    .teammateContainer {
-      display: flex;
-      flex-direction: row;
-      .teammateLink {
-        font-size: ${FONT_SIZE.font_20};
-        padding: 0.5rem;
-      }
+
+    .margin {
+      font-weight: 400;
+      margin: 0 0 1.5rem;
+    }
+
+    > p {
+      font-weight: 700;
+      margin: 0 0 0.5rem;
+    }
+  }
+
+  .team {
+    display: flex;
+    flex-flow: row;
+    gap: 0.75rem;
+
+    &:nth-of-type(1) {
+      margin: 0 0 0.5rem;
+    }
+  }
+
+  @media (max-width: 64rem) {
+    .footer_wrapper,
+    .copyright {
+      flex-flow: column;
+      gap: 0;
+    }
+
+    .part {
+      width: 100%;
     }
   }
 `;
@@ -55,35 +85,35 @@ export const Footer = (): JSX.Element => {
   return (
     <>
       <FooterStyled ref={footerRef}>
-        <footer>
-          <div className="fotterText">
-            <p className="partTitle">팀원 깃허브 링크</p>
-            <p className="partTitle">BE</p>
-            <div className="teammateContainer">
-              <a className="teammateLink" href="https://github.com/C5ng">
-                공대표
-              </a>
-              <a className="teammateLink" href="https://github.com/Jeonni">
-                이지연
-              </a>
-              <a className="teammateLink" href="https://github.com/Jung-seo">
-                서정욱
-              </a>
+        <div className="footer_wrapper">
+          <div className="part">
+            <p className="title">Team Name</p>
+            <p className="margin">동년배들 다 코딩한다.</p>
+            <p className="title">Project Term</p>
+            <p className="margin">2023-08-24 ~ 09-22</p>
+          </div>
+
+          <div className="part">
+            <p className="title">GitHub</p>
+
+            <div className="team">
+              <p>BE</p>
+              <a href="https://github.com/C5ng">공대표</a>
+              <a href="https://github.com/Jeonni">이지연</a>
+              <a href="https://github.com/Jung-seo">서정욱</a>
             </div>
-            <p className="partTitle">FE</p>
-            <div className="teammateContainer">
-              <a className="teammateLink" href="https://github.com/sintobury">
-                허찬욱
-              </a>
-              <a className="teammateLink" href="https://github.com/eg1377">
-                박다해
-              </a>
-              <a className="teammateLink" href="https://github.com/pearl-sea">
-                박진주
-              </a>
+
+            <div className="team">
+              <p>FE</p>
+              <a href="https://github.com/sintobury">허찬욱</a>
+              <a href="https://github.com/eg1377">박다해</a>
+              <a href="https://github.com/pearl-sea">박진주</a>
             </div>
           </div>
-        </footer>
+        </div>
+        <div className="copyright">
+          <p>WonPrice @ 2023. All rights reserved.</p>
+        </div>
       </FooterStyled>
     </>
   );
