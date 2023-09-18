ca01ecc1ca835d7a2cc282ec1f257b070e7df260
pearlsea <sakanaplanet@gmail.com>
2023-09-18 11:09:58 +0900

    feat: 글 등록시 과거 선택 못하게 수정

--- client/src/util/date.ts
+++ client/src/util/date.ts
@@ -3,3 +3,13 @@ export const formatTime = (time: string | undefined) => {
     return time.replace("T", " ");
   }
 };
+
+export const getCurrentDateTime = () => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, "0");
+  const day = String(now.getDate()).padStart(2, "0");
+  const hours = String(now.getHours()).padStart(2, "0");
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
+};
