4a572e2ef74551dd1f7c0e78e457de2878e4b887
sintobury <herkongho@gmail.com>
2023-09-19 09:45:11 +0900

    login 반응형 수정

--- client/src/util/productStatus.ts
+++ client/src/util/productStatus.ts
@@ -5,8 +5,6 @@ export const translateProductStatus = (productStatus: string): string => {
     return "구매자와 거래 진행중";
   } else if (productStatus === "AFTER") {
     return "거래 완료";
-  } else if (productStatus === "CANCLE") {
-    return "거래 파기";
   }
   return ""; //알맞은 값이 안 들어왔을 때
 };
